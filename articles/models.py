df3376e66e5f274b2d1b186472f5b9993a4bf545
Semih Orhan <sorhan3.14@gmail.com>
2023-07-06 11:13:27 +0300

    create templates for views

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class Article(models.Model):
+    id = models.IntegerField()
     title = models.TextField()
     content = models.TextField()
--- trydjango/views.py
+++ trydjango/views.py
@@ -1,11 +1,32 @@
+"""
+To render html web pages
+"""
+import random
 from django.http import HttpResponse
-
+from django.template.loader import render_to_string
 from articles.models import Article
 
+
 def home_view(request):
-    article_obj = Article.objects.get(id=1)
-    html = f'''
-        <h1>{article_obj.title}</h1>
-        <p>{article_obj.content}</p>
-    '''
-    return HttpResponse(html)
+    """
+    Take in a request (Django sends request)
+    Return HTML as a response (We pick to return the response)
+    """
+    random_id = random.randint(1, 4) # pseudo random
+    
+    # from the database??
+    article_obj = Article.objects.get(id=random_id)
+
+    context = {
+        "object": article_obj,
+        "title": article_obj.title,
+        "id": article_obj.id,
+        "content": article_obj.content
+    }
+    # Django Templates
+    HTML_STRING = render_to_string("home-view.html", context=context)
+    # HTML_STRING = """
+    # <h1>{title} (id: {id})!</h1>
+    # <p>{content}!</p>
+    # """.format(**context)
+    return HttpResponse(HTML_STRING)
